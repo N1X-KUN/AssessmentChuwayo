5a8a7cc2016779284365c34757252bbfec09cd25
Gerard Matthew <gerrymf55@gmail.com>
2026-03-03 21:13:08 +0400

    slight progress very little

--- My project/Assets/FoodItem.cs
+++ My project/Assets/FoodItem.cs
@@ -6,6 +6,7 @@ public class FoodItem : MonoBehaviour
     [Header("Food Data")]
     public string currentWord;
     public string originalWord; 
+    public bool isRotten = false; 
     
     private TMP_Text floatingText; 
     private SpriteRenderer foodImage;
@@ -16,22 +17,54 @@ public class FoodItem : MonoBehaviour
     public float fallSpeed = 2f;       
     public float groundYLevel = -2.5f; 
 
+    [Header("Dizzy Effect Settings")]
+    public float shakeIntensity = 0.05f; 
+    public float scrambleSpeed = 0.1f;   
+
     private bool hasHitGround = false;
+    private Vector3 textStartingLocalPos; 
+    private float scrambleTimer = 0f;
+    private bool isCurrentlyDizzy = false;
 
     void Awake()
     {
         floatingText = GetComponentInChildren<TMP_Text>();
         foodImage = GetComponent<SpriteRenderer>();
+
+        if (floatingText != null)
+        {
+            textStartingLocalPos = floatingText.transform.localPosition;
+        }
     }
 
-    public void SetupFood(string newWord, Sprite newSprite, WordManager manager)
+    public void SetupFood(string newWord, Sprite newSprite, WordManager manager, bool makeRotten = false)
     {
         currentWord = newWord;
         originalWord = newWord; 
         wordManager = manager;
+        isRotten = makeRotten;
         
         if (floatingText != null) floatingText.text = currentWord;
-        if (foodImage != null && newSprite != null) foodImage.sprite = newSprite;
+        
+        if (foodImage != null && newSprite != null) 
+        {
+            foodImage.sprite = newSprite;
+        }
+
+        // --- THE POISON FIX ---
+        if (isRotten)
+        {
+            // Use pure, bright neon green to punch through dark sprites!
+            if (foodImage != null) foodImage.color = Color.green; 
+            // Turn the text green so the player CANNOT miss it!
+            if (floatingText != null) floatingText.color = Color.green; 
+        }
+        else 
+        {
+            // Make sure normal food is perfectly white/clear
+            if (foodImage != null) foodImage.color = Color.white; 
+            if (floatingText != null) floatingText.color = Color.white; 
+        }
     }
 
     void Update()
@@ -46,7 +79,7 @@ public class FoodItem : MonoBehaviour
             {
                 hasHitGround = true;
                 
-                if (wordManager != null)
+                if (wordManager != null && !isRotten)
                 {
                     wordManager.MissedFood(this, originalWord);
                 }
@@ -54,11 +87,70 @@ public class FoodItem : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+
+        bool managerSaysDizzy = (wordManager != null && wordManager.isPlayerDizzy);
+
+        if (managerSaysDizzy)
+        {
+            isCurrentlyDizzy = true; 
+            
+            if (floatingText != null)
+            {
+                float shakeX = Random.Range(-shakeIntensity, shakeIntensity);
+                float shakeY = Random.Range(-shakeIntensity, shakeIntensity);
+                floatingText.transform.localPosition = textStartingLocalPos + new Vector3(shakeX, shakeY, 0);
+                
+                scrambleTimer -= Time.deltaTime;
+                if (scrambleTimer <= 0)
+                {
+                    floatingText.text = ScrambleWord(currentWord);
+                    scrambleTimer = scrambleSpeed; 
+                }
+            }
+        }
+        else
+        {
+            if (isCurrentlyDizzy)
+            {
+                isCurrentlyDizzy = false; 
+                
+                if (floatingText != null)
+                {
+                    floatingText.transform.localPosition = textStartingLocalPos;
+                    floatingText.text = currentWord; 
+                }
+            }
+        }
+    }
+
+    private string ScrambleWord(string word)
+    {
+        char[] chars = word.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            int randomIndex = Random.Range(0, chars.Length);
+            char temp = chars[i];
+            chars[i] = chars[randomIndex];
+            chars[randomIndex] = temp;
+        }
+        return new string(chars);
     }
 
     public void RemoveFirstLetter()
     {
         currentWord = currentWord.Substring(1);
-        if (floatingText != null) floatingText.text = currentWord;
+        
+        if (wordManager != null && wordManager.isPlayerDizzy)
+        {
+            if (floatingText != null) 
+            {
+                floatingText.text = ScrambleWord(currentWord);
+            }
+            scrambleTimer = scrambleSpeed; 
+        }
+        else if (floatingText != null)
+        {
+            floatingText.text = currentWord;
+        }
     }
 }
--- My project/Assets/WordManager.cs
+++ My project/Assets/WordManager.cs
@@ -1,5 +1,13 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class FoodEntry
+{
+    public string word;
+    public Sprite foodSprite;
+}
 
 public class WordManager : MonoBehaviour
 {
@@ -8,20 +16,34 @@ public class WordManager : MonoBehaviour
     public Transform spawnPoint;
     public KommyController kommy;
 
-    [Header("Spawning Settings")]
-    public string[] wordDatabase = { "apple", "banana", "pear", "peach", "grape" };
+    [Header("Food Database (Put all Fruits, Meats, Veggies here!)")]
+    public List<FoodEntry> allFoods = new List<FoodEntry>();
     public float spawnRate = 2.5f;
+
+    [Header("Rotten Food Settings")]
+    public bool canDropRottenFood = false; 
+    public float rottenChance = 30f; 
+    public GameObject dizzyOverlay; 
+    public float dizzyDuration = 4f; 
 
     [Header("Typing Mechanics")]
     private List<FoodItem> activeFoods = new List<FoodItem>(); 
     private FoodItem targetedFood = null; 
     
-    // THE THIEF'S POCKET
-    public List<string> thiefPocket = new List<string>();
+    public List<FoodEntry> thiefPocket = new List<FoodEntry>();
+
+    [HideInInspector]
+    public bool isPlayerDizzy = false; 
 
     void Start()
     {
-        thiefPocket.AddRange(wordDatabase);
+        foreach (var food in allFoods)
+        {
+            food.word = food.word.ToLower();
+        }
+        
+        thiefPocket.AddRange(allFoods);
+        if (dizzyOverlay != null) dizzyOverlay.SetActive(false);
     }
 
     public void StartSpawning()
@@ -36,23 +58,47 @@ public class WordManager : MonoBehaviour
 
     void SpawnFood()
     {
-        if (thiefPocket.Count == 0) return;
+        // 1. If the pocket is empty, wait for him to reload!
+        if (thiefPocket.Count == 0) return; 
 
+        // 2. Roll the dice to see if the food he drops will be poisoned
+        bool droppingRotten = false;
+        if (canDropRottenFood)
+        {
+            float randomRoll = Random.Range(0f, 100f);
+            if (randomRoll <= rottenChance)
+            {
+                droppingRotten = true;
+            }
+        }
+
+        // 3. Reach into the pocket and pull out a normal food (like Apple or Melon)
         int randomIndex = Random.Range(0, thiefPocket.Count);
-        string chosenWord = thiefPocket[randomIndex];
-        
+        FoodEntry chosenEntry = thiefPocket[randomIndex];
         thiefPocket.RemoveAt(randomIndex);
 
+        // 4. Spawn it!
         GameObject newFoodObj = Instantiate(foodPrefab, spawnPoint.position, Quaternion.identity);
         FoodItem newFood = newFoodObj.GetComponent<FoodItem>();
         
-        newFood.SetupFood(chosenWord, null, this);
+        // 5. Send it to the FoodItem! 
+        // If droppingRotten is TRUE, the FoodItem script automatically tints this sprite Dark Green!
+        newFood.SetupFood(chosenEntry.word, chosenEntry.foodSprite, this, droppingRotten);
         activeFoods.Add(newFood); 
     }
 
     public void MissedFood(FoodItem missedFood, string originalWord)
     {
-        thiefPocket.Add(originalWord);
+        // Only normal, safe foods go back into the pocket. Traps vanish forever!
+        if (!missedFood.isRotten)
+        {
+            FoodEntry originalEntry = allFoods.Find(x => x.word == originalWord);
+            if (originalEntry != null)
+            {
+                thiefPocket.Add(originalEntry);
+            }
+        }
+        
         activeFoods.Remove(missedFood);
         
         if (targetedFood == missedFood)
@@ -92,6 +138,17 @@ public class WordManager : MonoBehaviour
         {
             if (targetedFood.currentWord[0] == letter)
             {
+                if (targetedFood.isRotten)
+                {
+                    kommy.TypeWrongLetter(); 
+                    StartCoroutine(TriggerDizzyEffect());
+                    
+                    activeFoods.Remove(targetedFood);
+                    Destroy(targetedFood.gameObject);
+                    targetedFood = null;
+                    return; 
+                }
+
                 kommy.TypeCorrectLetter(); 
                 targetedFood.RemoveFirstLetter();
 
@@ -107,5 +164,16 @@ public class WordManager : MonoBehaviour
                 kommy.TypeWrongLetter(); 
             }
         }
+    }
+
+    private IEnumerator TriggerDizzyEffect()
+    {
+        isPlayerDizzy = true; 
+        if (dizzyOverlay != null) dizzyOverlay.SetActive(true);
+        
+        yield return new WaitForSeconds(dizzyDuration);
+        
+        isPlayerDizzy = false; 
+        if (dizzyOverlay != null) dizzyOverlay.SetActive(false);
     }
 }
